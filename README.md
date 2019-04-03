6850962838a1a30e0ac416ad3dca8a143853cb0a
Tristan White <oliver.tristan@gmail.com>
2019-04-03 15:47:18 +0200

    Update library name and add to npm

--- README.md
+++ README.md
@@ -1,19 +1,20 @@
-# Dots
+# dotbot
 
-Dots is a simply library that let's create a randomised dotted background pattern on any given element on your website.
-### Example
+dotbot is a simple library that let's you create a randomised, dotted background-pattern on any given element on your website.
+
+## Example
 ![Example](example.png)
 
-To initiate Dots. simply include the library and call the dot function like so:
-<pre><code>var myDots = new dot({
-  selectorId: 'dots'
+To initiate dotbot. simply include the library and call the dot function like so:
+<pre><code>var myDots = new dotbot({
+  selectorId: "dotbot-selector-id"
 )};
 </code></pre>
 
 ## Options
 You can define almost every aspect of layout:
-<pre><code>var myDots = new dot({
-  selectorId: 'dots',
+<pre><code>var myDots = new dotbot({
+  selectorId: "dotbot-selector-id",
   parentPosition: "relative",
   colors: [
      "blue",
@@ -26,5 +27,4 @@ You can define almost every aspect of layout:
   radius: 0,
   opacity: 0.1,
   zIndex: -1
-});<code></pre>
-
+});</code></pre>
--- dotbot.js
+++ dotbot.js
@@ -1,7 +1,7 @@
 (function(){
 
 	// Define settings
-	const dot = function(opts) {
+	const dotbot = function(opts) {
 
 		// Merge default options with user options
 		function merge_options(obj1,obj2) {
@@ -15,7 +15,7 @@
 			}
 			return obj3;
 		}
-		this.options = merge_options(dot.defaults,opts);
+		this.options = merge_options(dotbot.defaults,opts);
 
 		// Define options
 		this.dot_selector = document.getElementById(this.options.selectorId);
@@ -32,31 +32,32 @@
 		this.generateDots();
 	};
 
-	dot.prototype.generateDots = function() {
+	// Generate dots
+	dotbot.prototype.generateDots = function() {
 		const dot_colors = this.dot_colors;
 		for (let i = 0; i < this.dot_number; i++) {
 			const random_color = dot_colors[Math.floor(Math.random() * dot_colors.length)];
 			const random_size = Math.floor(Math.random() * (this.dot_size_maximum - this.dot_size_minimum + 1)) + this.dot_size_minimum;
 			const position_left = Math.floor(Math.random() * (100 - 0 + 1)) + 0;
 			const position_top = Math.floor(Math.random() * (100 - 0 + 1)) + 0;
-			const dot = document.createElement("span");
+			const dotbot = document.createElement("span");
 			this.dot_selector.style.position = "relative";
-			dot.style.position = "absolute";
-			dot.style.background = random_color;
-			dot.style.borderRadius = this.dot_radius+"%";
-			dot.style.opacity = this.dot_opacity;
-			dot.style.pointerEvents = "none";
-			dot.style.zIndex = this.dot_zIndex;
-			dot.style.height = random_size+"px";
-			dot.style.width = random_size+"px";
-			dot.style.left = position_left+"%";
-			dot.style.top = position_top+"%";
-			this.dot_selector.appendChild(dot);
+			dotbot.style.position = "absolute";
+			dotbot.style.background = random_color;
+			dotbot.style.borderRadius = this.dot_radius+"%";
+			dotbot.style.opacity = this.dot_opacity;
+			dotbot.style.pointerEvents = "none";
+			dotbot.style.zIndex = this.dot_zIndex;
+			dotbot.style.height = random_size+"px";
+			dotbot.style.width = random_size+"px";
+			dotbot.style.left = position_left+"%";
+			dotbot.style.top = position_top+"%";
+			this.dot_selector.appendChild(dotbot);
 		}
 	};
 
 	// Attach defaults for plugin to the plugin itself
-	dot.defaults = {
+	dotbot.defaults = {
 		selectorId: '',
 		colors: [
 			"rgb(248, 231, 28)",
@@ -75,8 +76,8 @@
 		zIndex: -1
 	};
 
-	// make accessible globally
-	window.dot = dot;
+	// Make accessible globally
+	window.dotbot = dotbot;
 
 })();
 
