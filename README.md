81577b7fbaa33bdbbfd5064fbab9446803e79179
Tristan White <oliver.tristan@gmail.com>
2019-04-03 14:58:47 +0200

    Deploy

--- README.md
+++ README.md
@@ -1,1 +1,30 @@
-# dots
+# Dots
+
+Dots is a simply library that let's create a randomised dotted background pattern on any given element on your website.
+### Example
+![Example](example.png)
+
+To initiate Dots. simply include the library and call the dot function like so:
+<pre><code>var myDots = new dot({
+  selectorId: 'dots'
+)};
+</code></pre>
+
+## Options
+You can define almost every aspect of layout:
+<pre><code>var myDots = new dot({
+  selectorId: 'dots',
+  parentPosition: "relative",
+  colors: [
+     "blue",
+     "rgb(126, 211, 33)",
+     "#f06d06"
+  ],
+  sizeMin: 3,
+  sizeMax: 80,
+  dots: 35,
+  radius: 0,
+  opacity: 0.1,
+  zIndex: -1
+});<code></pre>
+
--- index.js
+++ index.js
@@ -1,41 +1,78 @@
 (function(){
 
-	// Load settings
-	var dot = function(opts) {
+	// Define settings
+	const dot = function(opts) {
 
 		// Merge default options with user options
 		function merge_options(obj1,obj2) {
-			var obj3 = {};
-			var attrname;
-			for (attrname in obj1) { obj3[attrname] = obj1[attrname]; }
-			for (attrname in obj2) { obj3[attrname] = obj2[attrname]; }
+			let obj3 = {};
+			let attrname;
+			for (attrname in obj1) {
+				obj3[attrname] = obj1[attrname];
+			}
+			for (attrname in obj2) {
+				obj3[attrname] = obj2[attrname];
+			}
 			return obj3;
 		}
-
 		this.options = merge_options(dot.defaults,opts);
-		this.selector = document.getElementById(opts.selector);
-		console.log(this.options);
-		generateDots(this);
+
+		// Define options
+		this.dot_selector = document.getElementById(this.options.selectorId);
+		this.dot_size_minimum = this.options.sizeMin;
+		this.dot_size_maximum = this.options.sizeMax;
+		this.dot_number = this.options.dots;
+		this.dot_radius = this.options.radius;
+		this.dot_opacity = this.options.opacity;
+		this.dot_zIndex = this.options.zIndex;
+		this.dot_colors = this.options.colors;
+		this.dot_parent_position = this.options.opacity;
+
+		// Execute dots
+		this.generateDots();
 	};
 
-
-	dot.prototype.optionss = function() {
-		console.log(this.options);
+	dot.prototype.generateDots = function() {
+		const dot_colors = this.dot_colors;
+		for (let i = 0; i < this.dot_number; i++) {
+			const random_color = dot_colors[Math.floor(Math.random() * dot_colors.length)];
+			const random_size = Math.floor(Math.random() * (this.dot_size_maximum - this.dot_size_minimum + 1)) + this.dot_size_minimum;
+			const position_left = Math.floor(Math.random() * (100 - 0 + 1)) + 0;
+			const position_top = Math.floor(Math.random() * (100 - 0 + 1)) + 0;
+			const dot = document.createElement("span");
+			this.dot_selector.style.position = "relative";
+			dot.style.position = "absolute";
+			dot.style.background = random_color;
+			dot.style.borderRadius = this.dot_radius+"%";
+			dot.style.opacity = this.dot_opacity;
+			dot.style.pointerEvents = "none";
+			dot.style.zIndex = this.dot_zIndex;
+			dot.style.height = random_size+"px";
+			dot.style.width = random_size+"px";
+			dot.style.left = position_left+"%";
+			dot.style.top = position_top+"%";
+			this.dot_selector.appendChild(dot);
+		}
 	};
-
-
-	// Build the editor
-	function generateDots(dot) {
-		console.log(dot.options);
-	}
 
 	// Attach defaults for plugin to the plugin itself
 	dot.defaults = {
-		selector: '',
+		selectorId: '',
+		colors: [
+			"rgb(248, 231, 28)",
+			"rgb(126, 211, 33)",
+			"rgb(212, 56, 79)",
+			"rgb(128, 185, 254)",
+			"rgb(245, 166, 35)",
+			"rgb(212, 65, 208)"
+		],
+		parentPosition: "relative",
 		sizeMin: 3,
 		sizeMax: 8,
-		dotNum: 15,
-		radius: 50
+		dots: 15,
+		radius: 50,
+		opacity: 1,
+		zIndex: -1
 	};
 
 	// make accessible globally
